42a963e8e1a70e4e88d1a7215769d902ab00c278
maggie <mjtice0@gmail.com>
2023-03-06 17:45:50 -0700

    "refactored CoursesPage component to be a function instead of class"

--- src/components/CoursesPage.js
+++ src/components/CoursesPage.js
@@ -1,44 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { getCourses } from "../api/courseApi";
 
-class CoursesPage extends React.Component {
-  state = {
-    courses: [],
-  };
-  //called immediately after the component is mounted
-  //component must be mounted before we setState
-  componentDidMount() {
-    getCourses().then(courses => this.setState({ courses: courses }));
-  }
+function CoursesPage() {
+  //declare value
+  const [courses, setCourses] = useState([]);
+//add second argument (dependency array) so it does not rerun everytime the function rerenders
+  useEffect(() => {
+    getCourses().then(_courses => setCourses(_courses));
+  }, []);
 
-  render() {
-    return (
-      <>
-        <h2>Courses</h2>
-        <table className="table">
-          <thead>
-            <tr>
-              <th>Title</th>
-              <th>Author ID</th>
-              <th>Category</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.courses.map(course => {
-              return (
-                <tr>
-                  {/* //td is table data */}
-                  <td>{course.title}</td>
-                  <td>{course.authorid}</td>
-                  <td>{course.category}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </>
-    );
-  }
+  return (
+    <>
+      <h2>Courses</h2>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Author ID</th>
+            <th>Category</th>
+          </tr>
+        </thead>
+        <tbody>
+          {courses.map(course=> {
+            return (
+              <tr key={course.id}>
+                <td>{course.title}</td>
+                <td>{course.authorId}</td>
+                <td>{course.category}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
+  );
 }
 
 export default CoursesPage;
