b311cf63d34e7516ad3b509567e91546b666dde6
maggie <mjtice0@gmail.com>
2023-03-08 10:11:50 -0700

    "Added form in ManageCoursePage component"

--- src/components/CoursesPage.js
+++ src/components/CoursesPage.js
@@ -1,19 +1,22 @@
 import React, { useState, useEffect } from "react";
 import { getCourses } from "../api/courseApi";
 import CourseList from "./CourseList";
-
+import { Link } from "react-router-dom";
 function CoursesPage() {
   //declare value
   const [courses, setCourses] = useState([]);
-//add second argument (dependency array) so it does not rerun everytime the function rerenders
+  //add second argument (dependency array) so it does not rerun everytime the function rerenders
   useEffect(() => {
-    getCourses().then(_courses => setCourses(_courses));
+    getCourses().then((_courses) => setCourses(_courses));
   }, []);
 
   return (
     <>
-      <h2>Courses</h2> 
-      <CourseList courses={courses}/>
+      <h2>Courses</h2>
+      <Link className="btn btn-primary" to="/course">
+        Add Course
+      </Link>
+      <CourseList courses={courses} />
     </>
   );
 }
